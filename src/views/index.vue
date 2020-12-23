ba5b9f04c7476d99cff2b4c7482c30b679fa3df8
brandNewZq <2267062244@qq.com>
2020-12-23 13:27:07 +0800

    11

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <div>代办理事务</div>
+
+
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
